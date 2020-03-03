9af139dfb1afebcce05b76578ddd9c14b38166e8
asaidel <asaidel@technisys.com>
2020-03-03 17:20:11 -0300

    upload client image in client edit page, minor fixes

--- src/app/components/clientes/form.component.ts
+++ src/app/components/clientes/form.component.ts
@@ -3,7 +3,9 @@ import {Cliente} from '../../model/Cliente';
 import {ClienteService} from '../../services/cliente.service';
 import {Router, ActivatedRoute} from '@angular/router';
 import swal from 'sweetalert2';
-import {Region} from '../../model/Region';
+import { HttpEventType } from '@angular/common/http';
+import { Region } from '../../model/Region';
+import { ModalService } from '../../services/modal.service';
 
 @Component({
   selector: 'app-form',
@@ -13,27 +15,61 @@ import {Region} from '../../model/Region';
 export class FormComponent implements OnInit {
 
   cliente: Cliente = new Cliente();
-  titulo: string = 'Crear Cliente';
+  clienteSeleccionado: Cliente;
+  tituloAccion: string;
+  imagenSeleccionada: File;
+  progreso: number = 0;
   errores: string[];
   regiones: Region[];
   page: string;
-  constructor(private clienteService: ClienteService, private router: Router, private activatedRoute: ActivatedRoute) { }
+  constructor(private clienteService: ClienteService, private router: Router, private activatedRoute: ActivatedRoute,
+    private modalService: ModalService ) { }
 
-  ngOnInit() {
+  ngOnInit() {   
     this.cargarCliente();
     this.clienteService.getRegiones().subscribe( regiones => {
       this.regiones = regiones;
-    });
+    });    
   }
   
   cargarCliente(): void {
       this.activatedRoute.params.subscribe(params => {
         const id = params.id;
         this.page = params.page;
-        if (id) {
-          this.clienteService.obtenerCliente(id).subscribe(cliente => this.cliente = cliente);
+        
+        if (id) {          
+          this.tituloAccion = 'Modificar';          
+          this.clienteService.obtenerCliente(id).subscribe(cliente => this.cliente = cliente);          
         }
+        else this.tituloAccion = 'Crear';
       });
+  }
+
+  seleccionarFoto(event) {
+    this.imagenSeleccionada = event.target.files[0];
+    this.progreso = 0;
+    if (this.imagenSeleccionada.type.indexOf('image') < 0) {
+      swal.fire('Error: upload', 'Debe seleccionar un tipo de foto valido', 'error');
+      this.imagenSeleccionada = null;
+    }
+  }
+
+  subirFoto() {
+    if (!this.imagenSeleccionada) {
+      swal.fire('Error: upload', 'Debe seleccionar una foto', 'error');
+    } else {
+      this.clienteService.subirFoto(this.imagenSeleccionada, this.cliente.id)
+        .subscribe(event => {
+          if (event.type === HttpEventType.UploadProgress) {
+            this.progreso = Math.round((event.loaded / event.total) * 100);
+          } else if (event.type === HttpEventType.Response) {
+            const response: any = event.body;
+            this.cliente = response.cliente as Cliente;
+            this.modalService.notificacion.emit(this.cliente);
+            swal.fire('Foto subida exitosamente!', `La foto se ha subido exitosamente: ${response.mensaje}`, 'success');
+          }
+        });
+    }
   }
 
   public crear(): void {
